9e7e250d3dcb2f3cf38bc630e7eb43c7b19d5ca4
vivien anglesio <vivien.anglesio@gmail.com>
2019-03-11 18:09:16 +0100

    fix debug mode

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-const {debuglog} = require('util')('twitter-webhooks');
-debuglog('DEBUG MODE ENABLED');
+const debug = require('util').debuglog('twitter-webhooks');
+debug('DEBUG MODE ENABLED');
 module.exports.userActivity = require('./lib/user-activity-webhook');
